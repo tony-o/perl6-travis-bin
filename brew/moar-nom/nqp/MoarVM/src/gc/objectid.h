2e51f3d3ba1c2f640e767fd913c36d3f920585fe
tony-o <tony.odell@live.com>
2016-12-09 00:00:06 +0000

    nightly automated build

--- brew/moar-nom/nqp/MoarVM/src/gc/objectid.h
+++
@@ -1,3 +0,0 @@
-MVMuint64 MVM_gc_object_id(MVMThreadContext *tc, MVMObject *obj);
-void * MVM_gc_object_id_use_allocation(MVMThreadContext *tc, MVMCollectable *item);
-void MVM_gc_object_id_clear(MVMThreadContext *tc, MVMCollectable *item);
