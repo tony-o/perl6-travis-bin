375f9ea50a20e76c7357ec56a32df48e58d86f7d
tony-o <tony.odell@live.com>
2017-07-22 00:00:03 +0000

    nightly automated build

--- brew/moar-nom/nqp/MoarVM/src/platform/inttypes.h
+++
@@ -1,8 +0,0 @@
-#ifdef _MSC_VER
-#include <msinttypes/inttypes.h>
-/* Print size_t values. */
-#define MVM_PRSz "Iu"
-#else
-#include <inttypes.h>
-#define MVM_PRSz "zu" /* C99 */
-#endif
--- brew/moar-nom/nqp/MoarVM/src/platform/sys.h
+++
@@ -1,4 +0,0 @@
-/* Tries to determine the number of logical CPUs available to the process.
- * May return 0 on error.
- */
-MVMuint32 MVM_platform_cpu_count(void);
