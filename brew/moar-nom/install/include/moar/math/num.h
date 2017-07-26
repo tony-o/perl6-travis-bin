43ffce742f9a4cb2bc422ee1966ed96ae13b4d95
tony-o <tony.odell@live.com>
2017-07-26 00:00:04 +0000

    nightly automated build

--- brew/moar-nom/install/include/moar/math/num.h
+++
@@ -1,4 +0,0 @@
-MVMint64 MVM_num_isnanorinf(MVMThreadContext *tc, MVMnum64 n);
-MVMnum64 MVM_num_posinf(MVMThreadContext *tc);
-MVMnum64 MVM_num_neginf(MVMThreadContext *tc);
-MVMnum64 MVM_num_nan(MVMThreadContext *tc);
