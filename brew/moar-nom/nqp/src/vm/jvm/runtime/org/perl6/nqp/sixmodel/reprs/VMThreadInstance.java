ccf20c50cc4d1b4f071b3ed4eaa35a9b9aa03bb8
tony-o <tony.odell@live.com>
2018-02-05 00:00:15 +0000

    nightly automated build

--- brew/moar-nom/nqp/src/vm/jvm/runtime/org/perl6/nqp/sixmodel/reprs/VMThreadInstance.java
+++
@@ -1,9 +0,0 @@
-package org.perl6.nqp.sixmodel.reprs;
-
-import java.lang.Thread;
-
-import org.perl6.nqp.sixmodel.SixModelObject;
-
-public class VMThreadInstance extends SixModelObject {
-	public Thread thread;
-}
