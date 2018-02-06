d7037481f4c05b1d8587054dbfeed4dea5307653
tony-o <tony.odell@live.com>
2018-02-06 00:00:05 +0000

    nightly automated build

--- brew/git_reference/MoarVM/src/6model/reprs/Uninstantiable.h
+++
@@ -1,7 +0,0 @@
-/* Representation used by Uninstantiables. */
-struct MVMUninstantiable {
-    MVMObject common;
-};
-
-/* Function for REPR setup. */
-const MVMREPROps * MVMUninstantiable_initialize(MVMThreadContext *tc);
--- brew/moar-nom/nqp/examples/loops.nqp
+++
@@ -1,8 +0,0 @@
-#! nqp
-# Example of a while loop
-
-my $i := 0;
-while $i < 10 {
-    say("i=$i");
-    $i++;
-}
